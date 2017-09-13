7029b08084d824c93dfbeecf7282b9f02b9b44c0
Sam Dickinson <sam.t.dickinson@gmail.com>
2017-09-13 18:00:22 +0100

    Added Weather API Call

--- headlines.py
+++ headlines.py
@@ -1,4 +1,8 @@
 import feedparser
+import json
+import urllib2
+import urllib
+
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -19,7 +23,26 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", publication=publication.upper(), articles=feed['entries'])
+    weather = get_weather("London, UK")
+    return render_template("home.html", 
+                           publication=publication.upper(),
+                           articles=feed['entries'],
+                           weather=weather)
+
+def get_weather(query):
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=79f31e50e17bdba0f6162b4e16a3ae27'
+    query = urllib.quote(query)
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":
+                   parsed["weather"][0]["description"],
+                   "temperature":parsed["main"]["temp"],
+                   "city":parsed["name"]
+                  }
+    return weather
 
 if __name__ == '__main__':
     APP.run(port=5000, debug=True)
