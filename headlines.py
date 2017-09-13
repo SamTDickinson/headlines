87118fa0cff07c8f8e09ab4a09be2469a8403565
Sam Dickinson <sam.t.dickinson@gmail.com>
2017-09-13 13:42:18 +0100

    POST Requests

--- headlines.py
+++ headlines.py
@@ -10,10 +10,10 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
-@APP.route("/")
+@APP.route("/", methods=['GET', 'POST'])
 def get_news():
     """ Returns the first RSS feed """
-    query = request.args.get("publication")
+    query = request.form.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
     else:
