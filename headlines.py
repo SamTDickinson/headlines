3ae175324c7ed06c28c3ce6d42624b73a01590ca
Sam Dickinson <sam.t.dickinson@gmail.com>
2017-09-14 13:31:20 +0100

    Added currency api with user selected dropdowns

--- headlines.py
+++ headlines.py
@@ -17,11 +17,13 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 DEFAULTS = {
     'publication' : 'bbc',
-    'city' : 'London, UK'
+    'city' : 'London, UK',
+    'currency_from' : 'GBP',
+    'currency_to' : 'USD'
 }
 
-WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?"+_
-"q={}&units=metric&APPID=79f31e50e17bdba0f6162b4e16a3ae27"
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=79f31e50e17bdba0f6162b4e16a3ae27"
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=a010a8aa4c284792bdfbfb4b0e9b2367"
 
 @APP.route("/")
 def home():
@@ -39,10 +41,24 @@ def home():
         city = DEFAULTS['city']
     weather = get_weather(city)
 
+    currency_from = request.args.get('currency_from')
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+
+    currency_to = request.args.get('currency_to')
+    if not  currency_to:
+        currency_to = DEFAULTS['currency_to']
+
+    rate, currencies = get_currency(currency_from, currency_to)
+
     return render_template("home.html",
                            publication=publication.upper(),
                            articles=articles,
-                           weather=weather)
+                           weather=weather,
+                           currency_from=currency_from,
+                           currency_to=currency_to,
+                           rate=rate,
+                           currencies=sorted(currencies))
 
 def get_news(query):
     """ Returns the RSS feed for requested or Default publication """
@@ -63,10 +79,20 @@ def get_weather(query):
     if parsed.get("weather"):
         weather = {"description":
                    parsed["weather"][0]["description"],
-                   "temperature":parsed["main"]["temp"],
-                   "city":parsed["name"]
+                   "temperature" : parsed["main"]["temp"],
+                   "city" : parsed["name"],
+                   "country" : parsed['sys']['country']
                   }
     return weather
+
+def get_currency(c_frm, c_to):
+    """ returns currency rates for requested of default value """
+    all_currency = urllib2.urlopen(CURRENCY_URL)
+
+    parsed = json.load(all_currency).get('rates')
+    frm_rate = parsed.get(c_frm.upper())
+    to_rate = parsed.get(c_to.upper())
+    return (to_rate/frm_rate, parsed.keys())
 
 if __name__ == '__main__':
     APP.run(port=5000, debug=True)
